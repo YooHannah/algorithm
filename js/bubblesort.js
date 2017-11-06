4deb59981475eab9f0350aae5ddcec5ea9e3dd73
YooHannah <1942063705@qq.com>
2017-11-06 17:09:01 +0800

    冒泡排序

--- js/bubblesort.js
+++ js/bubblesort.js
@@ -10,7 +10,7 @@ function BubbleSort(val){
   var list = val.slice();
   var flag = 1;//添加flag,当某个个元素在比较过程中两两之间没有发生交换，则说明之前的顺序已经被排好，不需要再比较，提前结束
   for(var i = 0;i<list.length-1 && flag;i++){
-    for(var j= list.length-1;j>i;j--){//每一轮把位置i后面最小的滚到前面来，下一轮循环时，排除之前排好序的i+1个数
+    for(var j= list.length-1;j>i;j--){//每一轮把位置i后面最小的滚到前面来,下一轮循环时,排除之前排好序的i+1个数
        flag = 0;
       if(list[j-1] >= list[j]){
         var temp = list[j-1];
