5573867dcbc30be96ecc90deccd07cea392fab53
YooHannah <1942063705@qq.com>
2017-11-16 18:29:45 +0800

    构造相关

---
+++ js/keypoint.js
@@ -0,0 +1,60 @@
+//1.
+function foo(){
+  foo.a=function(){console.log(1)};
+  this.a=function(){console.log(2)};
+  a=function(){console.log(3)};
+  var a = function(){console.log(4)}
+}
+foo.prototype.a=function(){
+  console.log(5)
+}
+foo.a=function(){
+  console.log(6)
+}
+foo.a()//6 访问静态属性
+var b=new foo();
+b.a();//2  实例属性存在不再访问原型属性
+foo.a();//1 静态属性 因为new 重新被覆盖
+
+//2.
+var a = 5;
+function test(){
+  a=0;
+  console.log(a);
+  console.log(this.a);
+  var a;
+  console.log(a)
+}
+test() //0 5 0
+new test()//0 undefined 0
+
+//3.
+var foo = 1;
+function bar(){
+  foo=10;
+  return
+  function foo(){}
+}
+console.log(foo);//1
+bar();
+console.log(foo);//1
+//function foo 在bar里面定义，所以foo这个名，其实变成了bar的作用域里面的变量，也就是bar的局部变量，foo=10,改的也是局部的，第二个console所以打印的还是bar函数外面的
+
+//4.
+function Foo(){
+  getName = function(){console.log(1)};
+  return this;
+}
+Foo.getName=function (){console.log(2)};
+Foo.prototype.getName = function(){console.log(3)};
+var getName= function(){console.log(4)};
+function getName(){console.log(5)};
+Foo.getName();//2
+getName();//4 最先解析的同名函数被同名变量覆盖
+Foo().getName(); //this 为window 1
+getName();//原来挂载的函数被覆盖 1
+new Foo.getName();//2 运算优先级.和()大于new,所以先执行Foo.getName()，再执行new
+new Foo().getName()//3  先执行new Foo(),返回Foo{},Foo.getName 为构造函数静态属性，只能构造函数访问,所以追溯至原型
+new new Foo().getName();//3 执行顺序为，先初始化Foo的实例化对象，然后将其原型上的getName函数作为构造函数再次new
+console.log('baidu' && 'google'); //google
+console.log('baidu' || 'google'); //百度
---
+++ js/numberaddcomma.js
@@ -0,0 +1,140 @@
+/* 数字格式化，整数部分，每三位加一个逗号 */
+str= '11100011.31';
+str1 = str.split('.')
+console.log(str1);
+str2 = '';
+length = str1[0].length;
+count = parseInt(length/3);
+console.log(count)
+if(length%3){
+  count++
+}
+console.log(count)
+for(var i=0;i<count;i++){
+    if(i!==count-1){
+       str2 = ','+ str1[0].slice(-3)+str2;
+       str1[0] = str1[0].slice(0,str1[0].length-3);
+    }else{
+      str2 = str1[0].slice(-3)+str2;
+    }
+
+}
+console.log(str2+'.'+str1[1])
+
+/* 数组随机排序 */
+var array = [1,2,3,4,5,6,7];
+function randsort(arr){
+  let len = arr.length;
+  for(let i=0;i<len;i++){
+    let index = parseInt(Math.random()*len);
+    let temp = arr[index];
+    arr[index] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+console.log(randsort(array))
+
+/* 寻找出现次数最多字符和它的个数*/
+var str = 'aashjkdnlosavropehjkknlp';
+function findmore(str){
+ let letters={};
+  for(let i=0;i<str.length;i++){
+    if(!letters[str[i]]){
+      letters[str[i]] =1;
+    }else{
+      letters[str[i]]++;
+    }
+  }
+  let maxcount = 0;
+  let letter = [];
+  for(let k in letters){
+    if(letters[k]>maxcount){
+      maxcount = letters[k];
+      letter = [];
+      letter.push(k)
+    }else if(letters[k] == maxcount){
+      letter.push(k)
+    }
+  }
+  return {
+    letters:letters,
+    maxcount:maxcount,
+    letter:letter
+  }
+}
+console.log(findmore(str));
+
+/* 数组去重 */
+function unique(arr){
+  let temp=[];
+  let obj={};
+  for(let i=0;i<arr.length;i++){
+    if(!obj[arr[i]]){
+      temp.push(arr[i]);
+      obj[arr[i]]=1;
+    }
+  }
+  return temp;
+}
+var arr=[1,2,3,4,2,2,3,7,8,9,4,5];
+console.log(unique(arr))
+
+/*little test*/
+var a = 5;
+function test(){
+  a=0;
+  console.log(a);
+  console.log(this.a);
+  var a;//如果给a赋初始值，则不会继续找全局里面的a
+  console.log(a)
+}
+test() //0 5 0
+new test()//0 undefined 0
+
+/*事件监听*/
+class EventEmitter {
+  /* TODO */
+  constructor() {
+    this.listeners = [];
+  }
+  on(eventName, func){ //建立监听事件，有的话，就把关联函数push进去,没有的话就新建监听事件
+    for(let i=0;i<this.listeners.length;i++){
+      if(this.listeners[i].name === eventName){
+          this.listeners[i].funcs.push(func);
+          return
+      }
+    }
+    this.listeners.push({
+      name:eventName,
+      funcs:[]
+    })
+    if(func){
+      this.listeners[this.listeners.length-1].funcs.push(func)
+    }
+  }
+  emit(eventName, ...args){ //触发监听事件
+    for(let j=0;j<this.listeners.length;j++){
+        if(this.listeners[j].name === eventName){
+            for(let k =0;k<this.listeners[j].funcs.length;k++){
+              this.listeners[j].funcs[k](args)
+            }
+        }
+    }
+  }
+  off(eventName, func){ //取消监听
+      for(let j=0;j<this.listeners.length;j++){
+        if(this.listeners[j].name === eventName){
+          if(func == undefined){ //整个事件不再监听
+            this.listeners.splice(j,1);
+           }
+          for(let k =0;k<this.listeners[j].funcs.length;k++){ //取消监听事件的指定关联函数
+                if(func === this.listeners[j].funcs[k]){
+                  this.listeners[j].funcs.splice(k,1);
+                  return
+                }
+            }
+        }
+      }
+  }
+}
