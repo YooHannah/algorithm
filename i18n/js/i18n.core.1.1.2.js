bbe556d088f8d2f2036525958ad7f8b9d114bf27
YooHannah <1942063705@qq.com>
2019-06-04 23:48:44 +0800

    i18n国际化语言切换

---
+++ i18n/js/i18n.core.1.1.2.js
@@ -0,0 +1,37 @@
+(function(global,factory,plug){
+  return global[plug] = factory.call(global)
+})(this, function () { 
+  var __LOCATES__ ={}
+  var __CORE__ = {
+    setEl:function(selector){
+      this.__el__ = document.querySelector(selector)
+      this.__lang__ = this.__el__.dataset.i18nLocate || this.__lang__;
+      return this
+    },
+    setLocate : function (locate) { 
+      this.__lang__ = locate;
+      return this
+    },
+    get : function (locate,key) {
+      if(arguments.length === 1){
+      	key = arguments[0]
+        locate = this.__lang__
+      }else{
+        key = arguments[1]
+        locate = arguments[0]
+      }
+      return (__LOCATES__[locate] && __LOCATES__[locate][key]) || ''
+    },
+    register : function(locate,object){//注册方言
+      __LOCATES__[locate] = object ||{}
+    },
+    render(){
+      var __this__ = this;
+      var eles = this.__el__.querySelectorAll('[data-i18n-config]')
+      eles.forEach((e) => {
+        e.innerText = __this__.get(e.dataset.i18nConfig)
+      });
+    }
+  }
+  return __CORE__
+ },"I18N")
---
+++ i18n/js/locates/i18n-zh_CN.js
@@ -0,0 +1,7 @@
+I18N.register("zh_CN",{
+  baseConfig : "基本配置",
+  userInfo : "用户信息",
+  orgManage : '角色管理',
+  roleManage : '权限管理',
+  systemManage : "系统管理"
+})
