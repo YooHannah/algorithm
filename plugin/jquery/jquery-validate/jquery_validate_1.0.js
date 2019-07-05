0cc213ffecb32f35f148afc709cd8eb81dca8739
CMRH/liuyh001 <liuyh001@cmrh.com>
2019-07-05 11:16:32 +0800

    接下来需要添加清除提示逻辑

--- plugin/jquery/jquery-validate/jquery_validate_1.0.js
+++ plugin/jquery/jquery-validate/jquery_validate_1.0.js
@@ -10,48 +10,62 @@
         let items = this[0].children.length?this[0].children:this[0]
         let real_rules = []
         let rulesLength = rules.length
+        let defaultCSS = "color:red;font-size:12px;padding-top: 5px;"
+        let addInfo = (el,config,info)=>{
+          let css = defaultCSS + (config.style?config.style:'')
+          el.css({border:'1px solid red'})
+          el.after("<div style='"+css+"'>"+config.label+info+"</div>")
+        }
         let ruleFunc = {
           'max':(e)=>{
-            let config = e.data.config
-            if(isNaN(Number(config))){
+            let {rule,config} = e.data
+            rule = Number(rule)
+            if(isNaN(rule)){
               throw 'the format that your configuration about MAX was wrong!'
               return false
             }
-            return true
+            if(e.target.value.length>rule){
+              addInfo($(e.target),config,'长度不能大于'+rule)
+            }
           },
           'min':(e)=>{
-            let config = e.data.config
-            
-            if(isNaN(Number(config))){
+            let {rule,config} = e.data
+            rule = Number(rule)
+            if(isNaN(rule)){
               throw 'the format that your configuration about MIN was wrong!'
               return false
             }
-            return true
+            if(e.target.value.length<rule){
+              addInfo($(e.target),config,'长度不能少于'+rule)
+            }
           },
           'required':(e)=>{
-            let config = e.data.config
-            console.log(e.target.value)
-            if(config != 'true' &&  config != 'false'){
+            let {rule,config} = e.data
+            if(rule != 'true' &&  rule != 'false'){
               throw 'the format that your configuration about REQUIRED was wrong!'
               return false
             }
+            if(!!rule && !e.target.value){
+              addInfo($(e.target),config,'不能为空')
+            }
           },
         }
-        let addInfo = ()=>{
-
-        }
+        
         for(let i=0;i<rules.length;i++){
           let itemRules = rules[i].rule.split('|')
           let ruleLength = itemRules.length
           for(let j=0;j<ruleLength;j++){
-            let [key,config] = itemRules[j].split(':')
+            let [key,value] = itemRules[j].split(':')
             if(!ruleFunc[key]){
               throw "sorry,this plugin don't surpport this keyword :" + key+" now!" 
               return false
             }
             let el = $("input[name='" + rules[i].name+"']")
-            let event = rules[i].defaultEvent || 'blur'
-            el.bind(event,{config:config},ruleFunc[key])
+            // if(key === 'required' && (!!value || value === undefined)){ //给必填项加'*'
+            //   el.after("<span style='color:red;margin-left: 5px;'>*</span>")
+            // }
+            let event = rules[i].defaultEvent || 'blur'//默认失焦事件
+            el.bind(event,{rule:value,config:rules[i]},ruleFunc[key]) //绑定事件
           }
         }
       }
