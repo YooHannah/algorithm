fdc143a481972d031eb7e8151bbd2e29a3b9c02a
CMRH/liuyh001 <liuyh001@cmrh.com>
2019-08-22 17:44:30 +0800

    jquery reg

--- jquery/class7.js
+++ jquery/class7.js
@@ -9,3 +9,40 @@
  * \1 反向引用 以子表达式开始，以子表达式结束
  * 例 /\brequire\((["'])(.+)\1\)/g.exec("var b = require('./a.js')")
  */
+
+/** vue 模板字符串扫描*/
+var template = '<div>{{name}}</div>'
+//标签规则
+var ncname = '[a-zA-Z]'
+var qnamecapture = '('+ncname+')'
+//匹配开始标签
+var startTagOpen = new RegExp('^<'+qnamecapture)
+//匹配结束标签
+//vue 编译器 词法分析 parseHTML
+function parseHTML(html){
+  var index = 0 //切割起点
+  while(html){//死循环 html 切割 直到
+    var startTagMatch = parseStartTag() //开始标签 父标签
+    console.log(html) //>{{name}}</div>
+    break
+  }
+  parseStartTag()
+  function parseStartTag(){
+    var start = html.match(startTagOpen) //token 词 单元
+    if(start){
+      var match={
+        tagName:start[1],//名称
+        attrs:[],//属性+指令
+        start:index//子标签
+      }
+      advance(start[0].length)
+    }
+    return match
+  }
+  function advance(n){
+    index +=n
+    html = html.substring(n)
+  }
+  //继续扫描 属性 指令 ui结构 检测是否标签完整闭合 是否是组件
+  parseHTML(template)
+}
