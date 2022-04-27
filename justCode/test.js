25d417b0413a37ca3810a1957896ff3635b0e027
YooHannah <1942063705@qq.com>
2022-04-27 22:51:24 +0800

    练习

--- justCode/test.js
+++ justCode/test.js
@@ -1,6 +1,10 @@
-const judge = (a,b) => {
-  const doubleA = a + a;
-  const doubleB = b + b;
-  return doubleA.includes(b) && doubleB.includes(a);
+const process = (i,j,m,n, arr) => {
+  if ( i > m || j > n) {
+    return Number.MAX_SAFE_INTEGER;
+  }
+  if(i + 1 === m && j === n || i === m && j + 1 === n) {
+    return arr[i][j];
+  }
+  return arr[i][j] + Math.min(process(i+1, j, m,n,arr), process(i,j+1,m,n,arr));
 }
-console.log(judge('ab2cd', 'bcda'));
+console.log(process(0,0,3,2,[[0,1,99],[2,10,84], [100,3,5],[80,2,7]]));
