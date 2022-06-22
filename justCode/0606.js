440cc85c7b748ab21e5c3b17255b3513df8fc80f
YooHannah <1942063705@qq.com>
2022-06-22 09:07:29 +0800

    练习

--- justCode/0606.js
+++ justCode/0606.js
@@ -461,3 +461,292 @@ const calculate = (str, pos) => {
  * 如果移除的字符组成的序列不一样，归为不同的方案
  * 
  */
+
+
+ /***0611 */
+
+ /**
+  * 动态规划状态依赖技巧
+  * 
+  * 给定一个正整数，求裂开的的方式有多少种
+  * 比如
+  * 1 => (1) 1种
+  * 2=> (1,1) (2) 2种
+  * 3=>(1,1,1)(1,2),(3) 3种
+  * 4=>(1,1,1,1)(1,1,2)(1,3)(2,2)(4) 5种
+  * 
+  */
+
+/**
+ * 给定一颗二叉树的头结点head
+ * 已知所有节点都不一样，返回其中最大的且符合搜索二叉树条件的最大拓扑结构大小
+ * 拓扑结构：不是子树，能连起来的结构都算
+ * 
+ */
+
+/**
+ * 给定一个长度为偶数的数组arr,长度记为2*N
+ * 前N个为左部分，后N个为右部分
+ * arr就可以表示为[l1,l2,l3,...ln,r1,r2,r3,...rn]
+ * 请将数组调整成[r1,l1,r2,l2,...rn,ln]的样子
+ * 
+ * 思路：
+ * 完美洗牌问题
+ * 根据变换规则可以知道
+ * 原来的每个数据最终会在什么位置，
+ * 但是在一轮交换数据过程中不能保证所有的数据都会轮到被交换
+ * 只能部分数据可以得到交换，然后再部分数据的到交换
+ * 为避免通过记录交换状态带来的性能问题
+ * 使用完美洗牌规律
+ * 如果整个数组的长度 2 * N = 3^k -1 ,其中k>=1
+ * 也就是M = 2 * N = 2,8,26，... 这样的偶数时
+ * 每次交换的数据可以从3^(k-1)的位置开始 数组位置从1开始
+ * 比如 M = 26时
+ * 进行交换的数据可以从1,3,9，开始
+ * 
+ * 比如现在M = 12， 它最接近的3^k - 1是8
+ * 那就先对未来的签8个数据进行交换
+ * 先找到12个数据的中间mid
+ * 将mid左边两个和右边4个做交换
+ * l1,l2,l3,l4,l5,l6,r1,r2,r3,r4,r5,r6 变成
+ * l1,l2,l3,l4,r1,r2,r3,r4,l5,l6,r5,r6
+ * 这样前8个就可以按照从1,3位置开始进行交换的处理
+ * 剩下l5,l6,r5,r6 ，最接近2
+ * l5,l6,r5,r6  变成 l5,r5,l6,r6 
+ * 处理 l5,r5, 交换一下
+ * 再处理l6,r6 交换一下
+ * 
+ * 扩展：
+ * 将一无序数组调整成[a,b,c,d,e,f,g,h....]的顺序
+ * a<=b>=c<=d>=e<=f>=g<=h
+ * 要求空间复杂度为O(1)
+ * 思路：
+ * 先将数组从小到大排序，
+ * 如果是偶数个直接使用洗牌思路
+ * 如果是奇数个，排除掉最小的一个数据
+ * 剩下的偶数个进行洗牌
+ * 洗完在最开始，再加上最小的数据
+ */
+ 
+const rotate = (arr, left, mid, half) => {
+  const leftPart = arr.slice(left + half, mid +1); //中点左边需要换的一段
+  const rightPart = arr.slice(mid+1, mid + half + 1);// 中点右边需要换的一段
+  const newPart = [...leftPart.reverse(), ...rightPart.reverse()].reverse()
+  arr.splice(left + half, newPart.length, ...newPart);
+ }
+
+ // 获取满足洗牌规律的最终位置
+ const getFinalPos = (pos, len) => {
+   const half = Math.floor(len/2);
+   if (pos <= half) {
+    return 2 * pos
+   } else {
+     return 2 * (pos - half) -1;
+   }
+
+   // return (2 * i) % (len + 1); // 上面逻辑优化
+ }
+ // 根据不同圈开始的位置，开始调换数据
+ const cycles = (arr, left,len, k) => {
+  const list = arr.slice(left, left + len);
+  for(let i = 0;i<k; i++) {
+    const pos = Math.pow(3,i);
+    let curr = getFinalPos(pos,len);
+    let originalValue = list[pos-1];
+    while(curr != pos) {
+      const temp = list[curr -1];
+      list[curr - 1] = originalValue;
+      originalValue = temp;
+      curr = getFinalPos(curr,len);
+    }
+    list[pos -1] = originalValue;
+  }
+  arr.splice(left, len, ...list);
+ }
+ // 对数组进行切片处理
+ const shuffle = (arr, left,right) => {
+   while(right - left + 1) {
+    const length = right - left +1;
+    let base = 3;
+    let k = 1;
+    while(Math.pow(base,k) - 1 <= length) {
+      k++;
+    }
+    k--;
+    base = Math.pow(base,k) - 1;
+    const mid = Math.floor((right+left)/2);
+    const half = base / 2;
+    rotate(arr, left, mid, half);
+    cycles(arr, left, base, k);
+    left = left + base;
+   }
+ }
+ const washCard = arr => {
+   const length = arr.length;
+   if (arr && length && !(length % 2)) {
+    shuffle(arr, 0, length -1);
+    return arr
+   }
+ }
+
+
+/**
+ * 判断一个由[a-z]字符构成的字符串和一个包含'.'和'*'通配符的字符串是否匹配
+ * '.'匹配任一单一字符，'*' 匹配0个或者多个字符
+ * 输入的字符串长度不会超过100，且不为空
+ * 
+ */
+
+/**
+ * 数组异或和的定义： 将数组中所有的数异或起来得到的值
+ * 给定一个整型数组arr, 其中可能有正数，负数，0，
+ * 求其中子数组的最大异或和
+ * 
+ * 
+ */
+
+ /**
+  * 给定一个数组，数组上每个位置的值，代表该位置气球的分数
+  * 打爆气球得分规则如下
+  * 1，如果被打气球左右两边还有气球，找到离他最近的气球，本次得分为三者分数乘积
+  * 2. 如果左右两侧气球都没有了，则本次分数为被打气球分数
+  * 3.如果左右两边任意一边气球被打完，另一边有气球，
+  * 则本次得分为有气球一方离被打气球最近的气球分数与被打气球分数乘积
+  * 4.打爆所有气球得分之和为总分数，所以打爆气球顺序可以决定不同总分数
+  * 求能获得的最大分数
+  */
+
+/**
+ * 汉诺塔要求把所有圆盘从左边移动到右边
+ * 现给定一个数组arr表示各个圆盘当前在哪个柱子上
+ * 1代表在左住上，2代表在中柱上，3代表在右柱上
+ * 例如[3,3,2,1],表示现在有2个盘子在右柱上，1个在中柱上，1个在左柱上
+ * 假如arr现在表示最优移动过程中的一个状态，返回是移动过程中的第几步
+ * 如果不是游戏中的一个状态，返回-1
+ * 
+ */
+
+ /**
+  * 0615
+  */
+
+/**
+ * 给定一个字符串str1和str2,求str1的子串中含有str2所有字符的最小字符长度
+ * 
+ * 例如str1 = 'abcde', str2 = 'ac'
+ * str1子串中‘abc’ 满足，包含str2,且长度最短，返回3
+ * str1 = '12345'， str2 = '344'
+ * str1子串中没有包含str2的，所以返回0
+ */
+
+/**
+ * LFU 缓存替换算法
+ * 
+ * 一个缓存结构需要实现如下功能
+ * set(key, value) 加入或者修改key对应的value
+ * get(key) 查询key对应的value
+ * 
+ * 但是缓存中最多放K条记录，如果新的第K+1条记录要加入，
+ * 就根据策略删掉一条记录，然后才能把新纪录加入
+ * 策略如下：
+ * 在缓存的K条记录中，
+ * 哪一个Key从进入缓存结构的时刻开始
+ * 被调用set或者get的次数最少的，就删除掉
+ * 如果最少次数有多个，删除上次调用发生时间最早的key
+ * 
+ * 实现整个结构，K作为参数给出
+ * 
+ */
+
+/***
+ * N个加油站组成一个环形，
+ * oil数组表示第i个加油站可以给加多少油
+ * dis数组表示第i个加油站到第i+1个加油站需要消耗的油量
+ * 请选择一个加油站，从这个加油站开始给没有的车加上可以加的油量后
+ * 开始依次通过N个加油站
+ * 问是否存在可以跑完N个加油站回到触发加油站的方案
+ * 存在的话，返回从第几个加油站出发
+ * 
+ */
+
+/**
+ * 一颗二叉树原本是搜索二叉树
+ * 但是其中有两个节点调换了位置
+ * 使得这颗二叉树不再是搜索二叉树
+ * 请找出这两个节点返回
+ * 
+ * 进阶：
+ * 如果在原问题中得到了这两个错误节点
+ * 当然可以通过交换节点的方法让整棵树重新成为搜索二叉树
+ * 但现在要求你不能这么做，而是在结构上完全交换两个节点的位置
+ * 实现调整函数
+ * 
+ */
+
+/**
+ * 平面内有n个矩形，
+ * 第i个矩形的左下角坐标为(x1[i],y1[i])
+ * 右上角坐标为(x2[i],y2[i])
+ * 如果两个或者多个矩形有公共区域则认为他们相互重叠
+ * 不考虑边界和角落
+ * 请计算出平面内重叠矩形数量最多的地方，
+ * 有多少矩形重叠
+ * 
+ * 思路：
+ * 二维空间的问题转换成一维的问题
+ * 将矩形重叠问题转换成线段重叠问题
+ * 先根据底边重叠情况找出高度上重叠最多的矩形
+ * 然后拿到这些矩形的宽度开始结尾位置
+ * 转换成线段重叠问题
+ * 
+ * 线段重叠问题处理思路：
+ * 将线段按开始位置从小到大排列
+ * 新建一个栈，用于存放当前重叠线段结束位置
+ * 如果当前线段开始位置大于栈中结束位置，将这些结束位置从栈中移除
+ * 操作过程中记录栈中数据达到的最大值的时候，就是线段重叠最多的情况
+ * 
+ * 高度重叠最多情况也可以使用相似思路
+ * 
+ */
+
+const maxCoverCount = (x1,y1,x2,y2) => {
+  const bottomLines = y1.slice().map((value, pos)=> ({
+    value,
+    pos 
+  })).sort((a,b) => a.value - b.value);
+  const heightStack = [];
+  let currentMaxHeight = [];
+  for(let i = 0;i< bottomLines.length; i++) {
+    const { value: currentbottomLine, pos } = bottomLines[i];
+    for(let j = 0;j < heightStack.length; j++) {
+      const beforeTopline = y2[heightStack[j]];
+      if(beforeTopline <= currentbottomLine) {
+        heightStack.splice(j,1)
+      }
+    }
+    heightStack.push(pos);
+    if (heightStack.length >= currentMaxHeight.length) {
+      currentMaxHeight = [...heightStack]
+    }
+  }
+  const leftLines = currentMaxHeight.map(pos=> ({
+    value: x1[pos],
+    pos
+  })).sort((a,b) => a.value - b.value);
+  const widthStack = [];
+  let currentMaxWidth = [];
+  for(let k = 0; k<leftLines.length; k++) {
+    const {value: currentLeft, pos} = leftLines[k];
+    for(let l = 0; l < widthStack.length; l++) {
+      const beforeRight = x2[widthStack[l]];
+      if (beforeRight <= currentLeft) {
+        widthStack.splice(l,1);
+      }
+    }
+    widthStack.push(pos)
+    if (widthStack.length >= currentMaxWidth.length) {
+      currentMaxWidth = [...widthStack]
+    }
+  }
+  return currentMaxWidth.length;
+ }
--- justCode/test.js
+++ justCode/test.js
@@ -16,8 +16,64 @@ const generateBinaryTree = (data, i) => { // 生成一颗二叉树
   } : null; 
   return node
 }
-const root = generateBinaryTree(list, i);
+// const root = generateBinaryTree(list, i);
+
+const rotate = (arr, left, mid, half) => {
+  const leftPart = arr.slice(left + half, mid +1); //中点左边需要换的一段
+  const rightPart = arr.slice(mid+1, mid + half + 1);// 中点右边需要换的一段
+  const newPart = [...leftPart.reverse(), ...rightPart.reverse()].reverse()
+  arr.splice(left + half, newPart.length, ...newPart);
+ }
+ const getFinalPos = (pos, len) => {
+   const half = Math.floor(len/2);
+   if (pos <= half) {
+    return 2 * pos
+   } else {
+     return 2 * (pos - half) -1;
+   }
+
+   // return (2 * i) % (len + 1); // 上面逻辑优化
+ }
+ const cycles = (arr, left,len, k) => {
+  const list = arr.slice(left, left + len);
+  for(let i = 0;i<k; i++) {
+    const pos = Math.pow(3,i);
+    let curr = getFinalPos(pos,len);
+    let originalValue = list[pos-1];
+    while(curr != pos) {
+      const temp = list[curr -1];
+      list[curr - 1] = originalValue;
+      originalValue = temp;
+      curr = getFinalPos(curr,len);
+    }
+    list[pos -1] = originalValue;
+  }
+  arr.splice(left, len, ...list);
+ }
+ const shuffle = (arr, left,right) => {
+   while(right - left + 1) {
+    const length = right - left +1;
+    let base = 3;
+    let k = 1;
+    while(Math.pow(base,k) - 1 <= length) {
+      k++;
+    }
+    k--;
+    base = Math.pow(base,k) - 1;
+    const mid = Math.floor((right+left)/2);
+    const half = base / 2;
+    rotate(arr, left, mid, half);
+    cycles(arr, left, base, k);
+    left = left + base;
+   }
+ }
+ const washCard = arr => {
+   const length = arr.length;
+   if (arr && length && !(length % 2)) {
+    shuffle(arr, 0, length -1);
+    return arr
+   }
+ }
 
 
-
-console.log(calculate('3*(4+2*(5-7)-34)+4', 0));
+console.log(washCard([1,2,3,4,5,6,7,8,9,10,11,12]));
